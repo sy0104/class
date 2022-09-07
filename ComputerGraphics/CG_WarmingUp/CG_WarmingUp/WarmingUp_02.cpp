0c680d03bb534876a21e62e58b9891c61f72ae3a
dirma <syys0104@gmail.com>
2022-09-07 15:13:11 +0900

    2번 추가

--- ComputerGraphics/CG_WarmingUp/CG_WarmingUp/WarmingUp_02.cpp
+++ ComputerGraphics/CG_WarmingUp/CG_WarmingUp/WarmingUp_02.cpp
@@ -5,6 +5,10 @@
 using namespace std;
 
 // 2. 파일에서 데이터 읽기
+void CountWords(vector<char>&, vector<char>&);
+void PrintTextFile(vector<char>&);
+void ReverseSentence(vector<char>&);
+void ChangeWords(vector<char>&, char, char);
 
 int main()
 {
@@ -12,13 +16,13 @@ int main()
 	int wordCnt = 0, numCnt = 0;
 	bool isNum = false;
 
+	// 파일 읽기, 단어 세기
 	cout << "input data file name: ";
 	cin >> fileName;
 
 	ifstream in{ fileName };
 
 	vector<char> textFile;
-	vector<char> words;
 	vector<char> numbers;
 	char c;
 
@@ -27,8 +31,60 @@ int main()
 		textFile.push_back(c);
 	}
 
-	// 숫자 아스키코드 48 ~ 57
-	for (auto i = textFile.begin(); i != textFile.end(); ++i)
+	CountWords(textFile, numbers);
+	system("pause");
+	system("cls");
+
+	// 명령어 입력
+	char input;
+	char oldChar, newChar;
+	while (true)
+	{
+		PrintTextFile(textFile);
+		cout << "command: ";
+		cin >> input;
+		if (input == 'g' || input == 'G')
+		{
+			cout << "바꿀 문자, 새롭게 입력할 문자: ";
+			cin >> oldChar >> newChar;
+			cout << endl;
+		}
+
+		switch (input)
+		{
+		case 'd': case 'D':	// 문장 전체를 뒤집기
+			ReverseSentence(textFile);
+			break;
+		case 'e': case 'E':	// 동일한 간격으로 특정 문자를 일정 개수 삽입
+
+			break;
+		case 'f': case 'F':
+
+			break;
+		case 'g': case 'G':	// 문자 내부의 특정 문자를 다른 문자로 바꾸기
+			ChangeWords(textFile, oldChar, newChar);
+			break;
+		case 'h': case 'H':
+
+			break;
+		case 'q': case 'Q':
+			return 0;
+			break;
+		default:
+			continue;
+		}
+
+		system("pause");
+		system("cls");
+	}
+}
+
+void CountWords(vector<char>& text, vector<char>& numbers)
+{
+	int wordCnt = 0, numCnt = 0;
+	bool isNum = false;
+
+	for (auto i = text.begin(); i != text.end(); ++i)
 	{
 		cout << *i;	// 읽은 문자열 출력
 
@@ -41,7 +97,7 @@ int main()
 			if (*i == 10) numbers.push_back(' ');
 			else numbers.push_back(*i);
 
-			if (i + 1 == textFile.end())
+			if (i + 1 == text.end())
 				numbers.push_back(' ');
 
 			if (*i == ' ' || *i == 10)
@@ -49,7 +105,7 @@ int main()
 		}
 
 		// 단어 카운트
-		if (*i == ' ' || *i == 10 || i + 1 == textFile.end())
+		if (*i == ' ' || *i == 10 || i + 1 == text.end())
 			wordCnt++;
 	}
 
@@ -64,12 +120,68 @@ int main()
 		if (*i == ' ' || i + 1 == numbers.end())
 		{
 			numCnt++;
+			int s = 0;
 		}
 	}
-	
+
 	cout << endl << endl;
 	cout << "word count: " << wordCnt - numCnt << endl;
 	cout << "number count: " << numCnt << endl;
 	cout << endl;
+}
 
+void PrintTextFile(vector<char>& text)
+{
+	for (auto i = text.begin(); i != text.end(); ++i)
+		cout << *i;
+	cout << endl << endl;
+}
+
+void ReverseSentence(vector<char>& text)
+{
+	vector<char> s1, s2;
+	int idx = 0;
+
+	for (int i = 0; text[i] != 10; ++i)
+	{
+		s1.push_back(text[i]);
+	}
+
+	for (auto iter = s1.rbegin(); iter != s1.rend(); ++iter)
+	{
+		s2.push_back(*iter);
+		cout << s2[idx++];
+	}
+	
+	for (int i = 0; i < s2.size(); ++i)
+	{
+		text[i] = s2[i];
+	}
+
+	for (auto iter = text.begin(); iter != text.end(); ++iter)
+		cout << *iter;
+
+	cout << endl;
+}
+
+void ChangeWordsByInterval(vector<char>& text)
+{
+	// 3문자 후 2개의 @@ 삽입, 공백도 문자로 취급
+	int cnt = 0;
+
+	for (auto iter = text.begin(); iter != text.end(); ++iter)
+	{
+		
+	}
+}
+
+void ChangeWords(vector<char>& text, char oldChar, char newChar)
+{
+	for (auto iter = text.begin(); iter != text.end(); ++iter)
+	{
+		if (*iter == oldChar)
+			*iter = newChar;
+	}
+
+	PrintTextFile(text);
 }
