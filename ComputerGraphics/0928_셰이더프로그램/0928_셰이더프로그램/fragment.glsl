dd4c860ac12675902c5aef32dc7d1b895988288a
dirma <syys0104@gmail.com>
2022-09-28 15:58:04 +0900

    수업

---
+++ ComputerGraphics/0928_셰이더프로그램/0928_셰이더프로그램/fragment.glsl
@@ -0,0 +1,7 @@
+#version 330 core
+out vec4 color;
+
+void main()
+{
+	color = vecc4(1.0, 0.0, 0.0, 1.0);	// 빨간색 고정
+}
---
+++ ComputerGraphics/0928_셰이더프로그램/0928_셰이더프로그램/main.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <gl/glew.h> //--- 필요한 헤더파일 include
+#include <gl/freeglut.h>
+#include <gl/freeglut_ext.h>
+#include <random>
+#include <vector>
+using namespace std;
+
+void make_vertexShaders();
+void make_fragmentShaders();
+GLuint make_shaderProgram();
+GLvoid drawScene();
+GLvoid Reshape(int w, int h);
+GLint width, height;
+GLuint shaderID; //--- 세이더 프로그램 이름
+GLuint vertexShader; //--- 버텍스 세이더 객체
+GLuint fragmentShader; //--- 프래그먼트 세이더 객체
+void main(int argc, char** argv) //--- 윈도우 출력하고 콜백함수 설정
+{
+	width = 500;
+	height = 500;
+	//--- 윈도우 생성하기
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(width, height);
+	glutCreateWindow("Example1");
+	//--- GLEW 초기화하기
+	glewExperimental = GL_TRUE;
+	glewInit();
+	//--- 세이더 읽어와서 세이더 프로그램 만들기
+	make_vertexShaders(); //--- 버텍스 세이더 만들기
+	make_fragmentShaders(); //--- 프래그먼트 세이더 만들기
+	shaderID = make_shaderProgram(); //--- 세이더 프로그램 만들기
+	glutDisplayFunc(drawScene); //--- 출력 콜백 함수
+	glutReshapeFunc(Reshape);
+	glutMainLoop();
+}
+
+//--- 버텍스 세이더 객체 만들기
+void make_vertexShaders()
+{
+	GLchar* vertexSource;
+	//--- 버텍스 세이더 읽어 저장하고 컴파일 하기
+	//--- filetobuf: 사용자정의 함수로 텍스트를 읽어서 문자열에 저장하는 함수
+	vertexSource = filetobuf("vertex.glsl");
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexSource, NULL);
+	glCompileShader(vertexShader);
+	GLint result;
+	GLchar errorLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &result);
+	if(!result)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, errorLog);
+		cerr << "ERROR: vertex shader 컴파일 실패" << endl << errorLog << endl;
+		return false;
+	}
+}
+
+//--- 프래그먼트 세이더 객체 만들기
+void make_fragmentShaders()
+{
+	GLchar* fragmentSource;
+	//--- 프래그먼트 세이더 읽어 저장하고 컴파일하기
+	fragmentSource = filetobuf("fragment.glsl"); // 프래그세이더 읽어오기
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+	glCompileShader(fragmentShader);
+	GLint result;
+	GLchar errorLog[512];
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, errorLog);
+		cerr << "ERROR: fragment shader 컴파일 실패\n" << errorLog << endl;
+		return false;
+	}
+}
+
+GLuint make_shaderProgram()
+{
+	GLuint ShaderProgramID;
+	ShaderProgramID = glCreateProgram(); //--- 세이더 프로그램 만들기
+	
+	glAttachShader(ShaderProgramID, vertexShader); //--- 세이더 프로그램에 버텍스 세이더 붙이기
+	glAttachShader(ShaderProgramID, fragmentShader); //--- 세이더 프로그램에 프래그먼트 세이더 붙이기
+	
+	glLinkProgram(ShaderProgramID); //--- 세이더 프로그램 링크하기
+	
+	glDeleteShader(vertexShader); //--- 세이더 객체를 세이더 프로그램에 링크했음으로, 세이더 객체 자체는 삭제 가능
+	glDeleteShader(fragmentShader);
+	
+	glGetProgramiv(ShaderProgramID, GL_LINK_STATUS, &result); // ---세이더가 잘 연결되었는지 체크하기
+	if (!result) {
+		glGetProgramInfoLog(triangleShaderProgramID, 512, NULL, errorLog);
+		cerr << "ERROR: shader program 연결 실패\n" << errorLog << endl;
+		return false;
+	}
+	glUseProgram(ShaderProgramID); //--- 만들어진 세이더 프로그램 사용하기
+	//--- 여러 개의 세이더프로그램 만들 수 있고, 그 중 한개의 프로그램을 사용하려면
+	//--- glUseProgram 함수를 호출하여 사용 할 특정 프로그램을 지정한다.
+	//--- 사용하기 직전에 호출할 수 있다.
+	return ShaderProgramID;
+}
+
