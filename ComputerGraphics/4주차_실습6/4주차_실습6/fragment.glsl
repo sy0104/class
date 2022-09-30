3ce4a9a395870a9fbcfbb6d8c2aabb909beb777a
dirma10 <syys0104@gmail.com>
2022-10-01 03:27:55 +0900

    삼각형 4개 띄움

--- ComputerGraphics/4주차_실습6/4주차_실습6/fragment.glsl
+++ ComputerGraphics/4주차_실습6/4주차_실습6/fragment.glsl
@@ -1,7 +1,12 @@
 #version 330 core
-out vec4 color;
+
+// out_Color: vs에서 입력받는 색상 값
+// FragColor: 출력할 색상의 값으로 프레임 버퍼로 전달됨
+
+in vec3 out_Color;		// vs에게서 전달받음
+out vec4 FragColor;		// 색상 출력
 
 void main()
 {
-	color = vec4(1.0, 0.0, 0.0, 1.0);	// 빨간색 고정
+	FragColor = vec4(out_Color, 1.0);
 }
--- ComputerGraphics/4주차_실습6/4주차_실습6/vertex.glsl
+++ ComputerGraphics/4주차_실습6/4주차_실습6/vertex.glsl
@@ -1,6 +1,15 @@
 #version 330 core
 
+// in_Position: attribute index 0
+// in_Color: attribute index 1
+
+layout (location = 0) in vec3 in_Position;	// 위치 변수: attribute position 0
+layout (location = 1) in vec3 in_Color;		// 컬러 변수: attribute position 1
+
+out vec3 out_Color;		// fs에게 전달
+
 void main()
 {
-	gl_Position = vec4(0.5, 0.0, 0.0, 1.0);
+	gl_Position = vec4(in_Position.x, in_Position.y, in_Position.z, 1.0);
+	out_Color = in_Color;
 }
