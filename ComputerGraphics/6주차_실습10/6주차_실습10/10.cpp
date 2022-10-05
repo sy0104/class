f708ecdc01f59ee7b05ac301de45835a72f0c60c
dirma <syys0104@gmail.com>
2022-10-05 13:15:39 +0900

    실습 10번

--- ComputerGraphics/6주차_실습10/6주차_실습10/10.cpp
+++ ComputerGraphics/6주차_실습10/6주차_실습10/10.cpp
@@ -10,9 +10,6 @@
 #include <vector>
 using namespace std;
 
-enum DIR { LEFTUP, LEFTDOWN, RIGHTUP, RIGHTDOWN };
-enum SHAPE { SHAPE1, SHAPE2, SHAPE3, SHAPE4 };
-
 char* filetobuf(const char* file);
 void make_vertexShaders();
 void make_fragmentShaders();
@@ -25,15 +22,6 @@ void UpdateBuffer();
 
 GLvoid Keyboard(unsigned char, int, int);
 void TimerFunction(int);
-
-void InitDirSpeed();
-void Move();
-void CheckCollision();
-void Rotate(SHAPE, GLboolean);
-
-DIR dir = LEFTUP;
-GLfloat speed[3] = { 0.0, };
-GLboolean isBounce = true, isMove = true;
 
 GLuint width, height;
 GLuint shaderID; //--- 세이더 프로그램 이름
@@ -61,14 +49,14 @@ GLfloat colors[9][3] = { //--- 삼각형 꼭지점 색상
 void main(int argc, char** argv) //--- 윈도우 출력하고 콜백함수 설정
 {
 	width = 800;
-	height = 700;
+	height = 600;
 
 	//--- 윈도우 생성하기
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(width, height);
-	glutCreateWindow("Example1");
+	glutCreateWindow("Example10");
 
 	//--- GLEW 초기화하기
 	glewExperimental = GL_TRUE;
@@ -76,7 +64,6 @@ void main(int argc, char** argv) //--- 윈도우 출력하고 콜백함수 설정
 
 	InitShader();
 	InitBuffer();
-	InitDirSpeed();
 
 	glutDisplayFunc(DrawScene); //--- 출력 콜백 함수
 	glutReshapeFunc(Reshape);
@@ -197,12 +184,6 @@ GLvoid DrawScene() //--- 콜백 함수: 그리기 콜백 함수
 	// 사용할 VAO 불러오기
 	glBindVertexArray(VAO);
 
-	// 중앙 사각형 그리기
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-	glRectf(-0.35, -0.35, 0.35, 0.35);
-
-	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
 	// 삼각형 그리기
 	glDrawArrays(GL_TRIANGLES, 0, 9);	// 렌더링 할 인덱스 개수(9)
 
@@ -302,268 +283,14 @@ void InitShader()
 
 GLvoid Keyboard(unsigned char key, int x, int y)
 {
-	// 밖의 삼각형이 튕기기를 시작/멈춤
-	if (key == 'o' || key == 'O') isBounce = !isBounce;
 
-	// 안의 삼각형 이동/멈춤
-	else if (key == 'i' || key == 'I') isMove = !isMove;
 }
 
 void TimerFunction(int value)
 {
-	Move();
-	CheckCollision();
+
 	UpdateBuffer();
 
 	glutPostRedisplay();
 	glutTimerFunc(10, TimerFunction, 1);
-}
-
-void InitDirSpeed()
-{
-	random_device rd;
-	uniform_int_distribution<int> uid(0, 3);
-	uniform_real_distribution<float> urd(0.007, 0.01);
-
-	dir = (DIR)(uid(rd));
-
-	speed[0] = urd(rd);
-	speed[1] = speed[2] = urd(rd);
-	speed[2] *= -1;	// 위 삼각형은 처음에 좌측 이동
-}
-
-void Move()
-{
-	int idx = 0;
-
-	for (int i = 0; i < 3; ++i) {
-		idx = i * 3;
-
-		if (isBounce && i == 0) {	// 튕기는 삼각형
-			switch (dir)
-			{
-			case DIR::LEFTDOWN:
-				triShape[idx][0] -= speed[i];
-				triShape[idx++][1] -= speed[i];
-				triShape[idx][0] -= speed[i];
-				triShape[idx++][1] -= speed[i];
-				triShape[idx][0] -= speed[i];
-				triShape[idx++][1] -= speed[i];
-				break;
-			case DIR::LEFTUP:
-				triShape[idx][0] -= speed[i];
-				triShape[idx++][1] += speed[i];
-				triShape[idx][0] -= speed[i];
-				triShape[idx++][1] += speed[i];
-				triShape[idx][0] -= speed[i];
-				triShape[idx++][1] += speed[i];
-				break;
-			case RIGHTDOWN:
-				triShape[idx][0] += speed[i];
-				triShape[idx++][1] -= speed[i];
-				triShape[idx][0] += speed[i];
-				triShape[idx++][1] -= speed[i];
-				triShape[idx][0] += speed[i];
-				triShape[idx++][1] -= speed[i];
-				break;
-			case RIGHTUP:
-				triShape[idx][0] += speed[i];
-				triShape[idx++][1] += speed[i];
-				triShape[idx][0] += speed[i];
-				triShape[idx++][1] += speed[i];
-				triShape[idx][0] += speed[i];
-				triShape[idx++][1] += speed[i];
-				break;
-
-			}
-		}
-
-		else if (isMove && i > 0) {	// 사각형 내부 삼각형 - 좌우이동
-			triShape[idx][0] += speed[i];
-			triShape[idx + 1][0] += speed[i];
-			triShape[idx + 2][0] += speed[i];
-
-			if (speed[i] >= 0.0 && triShape[idx + 1][0] >= 0.35 ||
-				speed[i] <= 0.0 && triShape[idx][0] <= -0.35) {
-				// 반대 방향으로 이동
-				speed[i] *= -1.0;
-
-				// 색상 변경
-				GLfloat r, g, b;
-
-				random_device rd;
-				uniform_int_distribution<int> uid(0, 255);
-				r = (GLfloat)(uid(rd) / 255.0f);
-				g = (GLfloat)(uid(rd) / 255.0f);
-				b = (GLfloat)(uid(rd) / 255.0f);
-
-				colors[idx][0] = r;
-				colors[idx][1] = g;
-				colors[idx++][2] = b;
-				colors[idx][0] = r;
-				colors[idx][1] = g;
-				colors[idx++][2] = b;
-				colors[idx][0] = r;
-				colors[idx][1] = g;
-				colors[idx++][2] = b;
-			}
-		}
-	}
-}
-
-void CheckCollision()
-{
-	GLboolean isXCollision = false, isYCollision = false;
-
-	if (dir == LEFTDOWN) {
-		// 벽면 충돌
-		if (triShape[0][0] <= -1.0) Rotate(SHAPE2, true);
-		else if (triShape[0][1] <= -1.0 || triShape[0 + 1][1] <= -1.0) Rotate(SHAPE4, true);
-
-		// 내부 사각형 충돌
-		for (int i = 0; i < 3; ++i) {
-			// 오른쪽 벽면 충돌
-			if (triShape[i][0] <= 0.35 && triShape[i][0] >= 0.3) {
-				if ((triShape[0][1] >= -0.33 && triShape[0][1] <= 0.33) ||
-					(triShape[1][1] >= -0.33 && triShape[1][1] <= 0.33) ||
-					(triShape[2][1] >= -0.33 && triShape[2][1] <= 0.33)) Rotate(SHAPE2, false);
-			}
-
-			// 위쪽 벽면 충돌
-			if (triShape[i][1] >= 0.3 && triShape[i][1] <= 0.35) {
-				if ((triShape[0][0] >= -0.35 && triShape[0][0] <= 0.35) ||
-					(triShape[1][0] >= -0.35 && triShape[1][0] <= 0.35) ||
-					(triShape[2][0] >= -0.35 && triShape[2][1] <= 0.35)) Rotate(SHAPE4, false);
-			}
-		}
-
-	}
-	else if (dir == LEFTUP) {
-		// 벽면 충돌
-		if (triShape[0][0] <= -1.0) Rotate(SHAPE2, true);
-		else if (triShape[2][1] >= 1.0) Rotate(SHAPE1, true);
-
-		// 내부 사각형 충돌
-		for (int i = 0; i < 3; ++i) {
-			// 오른쪽 벽면 충돌
-			if (triShape[i][0] <= 0.35 && triShape[i][0] >= 0.3) {
-				if ((triShape[0][1] >= -0.33 && triShape[0][1] <= 0.33) ||
-					(triShape[1][1] >= -0.33 && triShape[1][1] <= 0.33) ||
-					(triShape[2][1] >= -0.33 && triShape[2][1] <= 0.33)) Rotate(SHAPE2, false);
-			}
-			// 아래쪽 벽면 충돌
-			if (triShape[i][1] >= -0.35 && triShape[i][1] <= -0.3) {
-				if ((triShape[0][0] >= -0.35 && triShape[0][0] <= 0.35) ||
-					(triShape[1][0] >= -0.35 && triShape[1][0] <= 0.35) ||
-					(triShape[2][0] >= -0.35 && triShape[2][0] <= 0.35)) Rotate(SHAPE1, false);
-			}
-		}
-	}
-	else if (dir == RIGHTDOWN) {
-		// 벽면 충돌
-		if (triShape[1][0] >= 1.0 || triShape[2][0] >= 1.0) Rotate(SHAPE3, true);
-		else if (triShape[0][1] <= -1.0 || triShape[1][1] <= -1.0) Rotate(SHAPE4, true);
-
-		// 내부 사각형 충돌
-		for (int i = 0; i < 3; ++i) {
-			// 왼쪽 벽면 충돌
-			if (triShape[i][0] >= -0.35 && triShape[i][0] <= -0.3) {
-				if ((triShape[0][1] >= -0.33 && triShape[0][1] <= 0.33) ||
-					(triShape[1][1] >= -0.33 && triShape[1][1] <= 0.33) ||
-					(triShape[2][1] >= -0.33 && triShape[2][1] <= 0.33))
-					Rotate(SHAPE3, false);
-			}
-			// 위쪽 벽면 충돌
-			if (triShape[i][1] >= 0.3 && triShape[i][1] <= 0.35) {
-				if ((triShape[0][0] >= -0.35 && triShape[0][0] <= 0.35) ||
-					(triShape[1][0] >= -0.35 && triShape[1][0] <= 0.35) ||
-					(triShape[2][0] >= -0.35 && triShape[2][0] <= 0.35))
-					Rotate(SHAPE4, false);
-			}
-		}
-	}
-	else if (dir == RIGHTUP) {
-		// 벽면 충돌
-		if (triShape[1][0] >= 1.0 || triShape[2][0] >= 1.0) Rotate(SHAPE3, true);
-		else if (triShape[2][1] >= 1.0) Rotate(SHAPE1, true);
-
-		// 내부 사각형 충돌
-		for (int i = 0; i < 3; ++i) {
-			// 왼쪽 벽면 충돌
-			if (triShape[i][0] >= -0.35 && triShape[i][0] <= -0.3) {
-				if ((triShape[0][1] >= -0.33 && triShape[0][1] <= 0.33) ||
-					(triShape[1][1] >= -0.33 && triShape[1][1] <= 0.33) ||
-					(triShape[2][1] >= -0.33 && triShape[2][1] <= 0.33)) Rotate(SHAPE3, false);
-			}
-			// 아래쪽 벽면 충돌
-			if (triShape[i][1] >= -0.35 && triShape[i][1] <= -0.3) {
-				if ((triShape[0][0] >= -0.35 && triShape[0][0] <= 0.35) ||
-					(triShape[1][0] >= -0.35 && triShape[1][0] <= 0.35) ||
-					(triShape[2][0] >= -0.35 && triShape[2][0] <= 0.35)) Rotate(SHAPE1, false);
-			}
-
-		}
-	}
-}
-
-void Rotate(SHAPE s, GLboolean isEdgeCollision)
-{
-	GLfloat x, y;
-
-	switch (s)
-	{
-	case SHAPE::SHAPE1:
-		x = triShape[0][0];
-		if (isEdgeCollision) y = 1.0;
-		else y = -0.35;
-
-		triShape[0][1] = y;
-		triShape[1][0] = x + 0.1;
-		triShape[1][1] = y - 0.3;
-		triShape[2][0] = x + 0.2;
-		triShape[2][1] = y;
-		break;
-	case SHAPE::SHAPE2:
-		if (isEdgeCollision) x = -1.0;
-		else x = 0.35;
-		y = triShape[0][1];
-
-		triShape[0][0] = x;
-		triShape[1][0] = x + 0.3;
-		triShape[1][1] = y + 0.1;
-		triShape[2][0] = x;
-		triShape[2][1] = y + 0.2;
-		break;
-	case SHAPE::SHAPE3:
-		if (isEdgeCollision) x = 1.0;
-		else x = -0.35;
-		y = triShape[0][1];
-
-		triShape[0][0] = x - 0.3;
-		triShape[1][0] = x;
-		triShape[1][1] = y - 0.1;
-		triShape[2][0] = x;
-		triShape[2][1] = y + 0.1;
-		break;
-	case SHAPE::SHAPE4:
-		x = triShape[0][0];
-		if (isEdgeCollision) y = -1.0;
-		else y = 0.35;
-
-		triShape[0][1] = y;
-		triShape[1][0] = x + 0.2;
-		triShape[1][1] = y;
-		triShape[2][0] = x + 0.1;
-		triShape[2][1] = y + 0.3;
-		break;
-	}
-
-	// 방향 전환
-	random_device rd;
-	uniform_int_distribution<int> uid(0, 3);
-
-	dir = (DIR)(uid(rd));
-
-	//while (dir != (DIR)(uid(rd)))
-	//	dir = (DIR)(uid(rd));
 }
